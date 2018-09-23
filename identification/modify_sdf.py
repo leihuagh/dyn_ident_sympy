d72e6daf528ab46e3dbf9ed096c43b76f5315f21
Radian Azhar <radiang91@gmail.com>
2018-09-22 20:55:05 -0400

    make .yaml file

--- identification/modify_sdf.py
+++ identification/modify_sdf.py
@@ -2,7 +2,7 @@
 import xml.etree.ElementTree as ET
 import math
 
-def modify_sdf(rbt_def, sdp_x_std, frame_names = [], filename='psm.sdf'):
+def modify_sdf(rbt_def, sdp_x_std, frame_names = [], filename='psm.sdf', namespace = 'dvrk_psm', name = 'PSM1', filename_cfg='psm_friction.yaml'):
     frame_num = rbt_def.frame_num
 
     x = sdp_x_std
@@ -18,6 +18,9 @@ def modify_sdf(rbt_def, sdp_x_std, frame_names = [], filename='psm.sdf'):
 
     g = 0
 
+    myfile = open(filename_cfg, "w")
+    myfile.write(namespace + ": \n")
+    myfile.write("  " + name + ": \n")
     for i in range(frame_num):
         for link in model.findall('link'):
             if frame_names[i] == link.get('name'):
@@ -43,11 +46,68 @@ def modify_sdf(rbt_def, sdp_x_std, frame_names = [], filename='psm.sdf'):
                     mass.text = str(sdp_x_std[g])
                     g += 1
 
-                # If Friction Increment
                 if rbt_def.use_friction[i]:
                     g = g + fric_num
 
                 if rbt_def.use_Ia[i]:
                     g += 1
 
+                #myfile.write("\n")
+
     tree.write('psm_new.sdf')
+
+def add_trans_config(rbt_def, sdp_x_std, frame_names = [], namespace = 'dvrk_psm', name = 'PSM1', filename='psm_friction.yaml'):
+    frame_num = rbt_def.frame_num
+
+    x = sdp_x_std
+
+    if len(frame_names) == 0:
+        return
+
+    g = 0
+    myfile = open(filename, "w")
+    myfile.write(namespace + ": \n")
+    myfile.write("  " + name + ": \n")
+
+    for i in range(frame_num):
+        if frame_names[i] != ' ':
+            if rbt_def.use_inertia[i]:
+                g += 10
+
+            # If Friction is modeled
+            myfile.write("    " + frame_names[i] + ": \n")
+            if 'Coulomb' in rbt_def.friction_type and rbt_def.use_friction[i]:
+                z = "%f" % (sdp_x_std[g])
+                myfile.write("      " + "Fc: " + z + "\n")
+                g += 1
+            else:
+                myfile.write("      " + "Fc: " + "0\n")
+
+            if 'viscous' in rbt_def.friction_type and rbt_def.use_friction[i]:
+                z = "%f" % (sdp_x_std[g])
+                myfile.write("      " + "Fv: " + z + "\n")
+                g += 1
+            else:
+                myfile.write("      " + "Fv: " + "0\n")
+
+            if 'offset' in rbt_def.friction_type and rbt_def.use_friction[i]:
+                z = "%f" % (sdp_x_std[g])
+                myfile.write("      " + "Fo: " + z + "\n")
+                g += 1
+            else:
+                myfile.write("      " + "Fo: " + "0\n")
+
+            # If Motor Inertia is modeled
+            if rbt_def.use_Ia[i]:
+                z = "%f" % (sdp_x_std[g])
+                myfile.write("      " + "Ia: " + z + "\n")
+                g += 1
+
+            else:
+                myfile.write("      " + "Ia: " + "0\n")
+
+
+    myfile.close()
+
+
+
